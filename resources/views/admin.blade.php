ee5546a8341c4275fa3a9de8fb9251ef670f0bf9
abdelmajidafaki <afakiabdelmajid@gmail.com>
2024-07-12 05:12:03 +0100

    i 	modified:   .editorconfig 	modified:   composer.json 	modified:   composer.lock 	new file:   package-lock.json 	modified:   package.json 	new file:   public/css/app.css 	new file:   public/fonts/vendor/bootstrap-icons/bootstrap-icons.woff 	new file:   public/fonts/vendor/bootstrap-icons/bootstrap-icons.woff2 	new file:   public/js/app.js 	new file:   public/mix-manifest.json 	modified:   resources/css/app.css 	modified:   resources/js/app.js 	modified:   resources/js/bootstrap.js 	new file:   resources/sass/_variables.scss 	new file:   resources/sass/app.scss 	new file:   resources/views/admin.blade.php 	modified:   resources/views/welcome.blade.php 	modified:   routes/web.php 	modified:   webpack.mix.

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-default navbar-fixed-bottom px-4">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div >
+                <a class="navbar-brand fs-2 " href="#">Brand</a>
+            </div>
+            <div>
+                <a class="nav-link active fs-4" aria-current="page" href="#">logout</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="collapse" id="sidebar">
+
+    </div>
+
+</body>
+
+
+</html>
